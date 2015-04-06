c30072a350e1d00170a76abd0a876c2adf697569
Michel Julien <micheljrjulien@yahoo.com>
2015-04-06 19:58:54 -0400

    new nav added

--- app/views/candidates/addcandidate.blade.php
+++ app/views/candidates/addcandidate.blade.php
@@ -7,39 +7,47 @@
 
 @section('body')
 <div class="container-fluid">
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">New Adventures Employment</a>
-    </div>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="/adduser">Add User<span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Search Candidates</a></li>
+                <a class="navbar-brand" href="#">New Adventures Employment</a>
+            </div>
 
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="/logout">Logout</a></li>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="/candidates">View All Candidates</a></li>
+                    <li><a href="/candidates/addcandidate">Add Candidate</a></li>
+                    <li><a href="/allpayments">Payments</a></li>
 
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+
+                </ul>
+
+                {{Form::open(array('route'=>'candidates.index','class'=>'navbar-form navbar-left','method'=>'GET'))}}
+                <div class="form-group">
+
+
+                    {{Form::text('searchf',null,array('class'=>'form-control','id'=>'searchf','placeholder'=>'Search Candidate'))}}
+                </div>
+
+
+                {{Form::submit('Search',array('class'=>'btn btn-default'))}}
+
+
+
+                {{Form::close()}}
+
+
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/logout">Logout</a></li>
+
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 <div class="row">
 <div class="col-lg-8">
 <h1>Add A New Candidate</h1>
